cc938a654795b047a791084089b3cf18d8739c61
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-05 06:18:59 +0700

    #18 - Add updatePost in PostService  ✏️📗:octocat:⬆

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -6,6 +6,7 @@ use App\Repositories\PostRepository;
 use Exception;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 use InvalidArgumentException;
 
 class PostService
@@ -70,6 +71,34 @@ class PostService
     public function getById($id)
     {
         return $this->postRepository->getById($id);
+    }
+
+    /**
+     * Update post data
+     * Store to DB if there are no errors.
+     *
+     * @param array $data
+     * @return String
+     */
+    public function updatePost($data, $id)
+    {
+        $validator = Validator::make($data, [
+            'title' => 'bail|min:2',
+            'description' => 'bail|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            throw new InvalidArgumentException($validator->errors()->first());
+        }
+
+        DB::beginTransaction();
+
+        try {
+            $post = $this->postRepository->update($data, $id);
+
+        } catch (Exception $e) {
+            throw new InvalidArgumentException($e->getMessage());
+        }
     }
 
 }
