b517e00d7bb83e0a2fe67671b9d281563baabd66
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-05 06:13:03 +0700

    #8 - Add __construct in PostRepository  ✏️📗:octocat:⬆

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -2,11 +2,23 @@
 
 namespace App\Repositories;
 
+use App\Models\Post;
+
 class PostRepository
 {
     /**
      * @var Post
      */
     protected $post;
+
+    /**
+     * PostRepository constructor.
+     *
+     * @param Post $post
+     */
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
 
 }
